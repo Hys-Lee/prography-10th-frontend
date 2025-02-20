ebdbebb816cc9e624d97a89391a3437c32ea0957
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-02-20 20:19:16 +0900

    feat: 컴포넌트 책임 분리 및 구조 재배치

---
+++ src/assets/form-info/defaultValues.ts
@@ -0,0 +1,10 @@
+import { FormField } from './schema';
+const defaultForm: FormField = {
+  personalInfo: undefined,
+  name: undefined,
+  email: undefined,
+  phoneNumber: undefined,
+  applyingType: undefined,
+};
+
+export default defaultForm;
--- src/components/apply-form/ApplyForm.tsx
+++ src/components/apply-form/ApplyForm.tsx
@@ -1,83 +1,19 @@
-import { createFunnelSteps, useFunnel } from '@use-funnel/react-router';
 import { FormProvider, useForm } from 'react-hook-form';
-import PersonalInfo from './steps/PersonalInfo';
-import BasicInfo from './steps/BasicInfo';
-import ApplyingType from './steps/ApplyingType';
-import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
-import StepLayout from './steps/StepLayout';
 import { ERROR_COMMNETS } from '../../constants/errorComments';
 import { toast } from 'react-toastify';
 import { useMutation } from '@tanstack/react-query';
-import axios from 'axios';
 import { BounceDot } from 'basic-loading';
-import { size } from 'lodash';
 import Completion from './steps/Completion';
-
-const formSchema = z.object({
-  personalInfo: z
-    .enum(['agree', 'disagree'], { message: ERROR_COMMNETS.NON_CHECK })
-    .refine(
-      (value) => (value === 'agree' ? true : false),
-      ERROR_COMMNETS.SHOULD_AGREE
-    ),
-  name: z
-    .string({ message: ERROR_COMMNETS.EMPTY_STRING })
-    // .optional()
-    .refine(
-      (value) => typeof value === 'string' && value.length > 0,
-      ERROR_COMMNETS.EMPTY_STRING
-    ),
-  email: z
-    .string({ message: ERROR_COMMNETS.EMPTY_STRING })
-    .nonempty({ message: ERROR_COMMNETS.EMPTY_STRING })
-    .email(ERROR_COMMNETS.INVALID_TYPE)
-    // .optional()
-    .refine((value) => value !== undefined, ERROR_COMMNETS.EMPTY_STRING),
-  phoneNumber: z
-    .string({ message: ERROR_COMMNETS.EMPTY_STRING })
-    .nonempty({ message: ERROR_COMMNETS.EMPTY_STRING })
-    .regex(/^\d{3}-\d{4}-\d{4}$/, ERROR_COMMNETS.INVALID_TYPE)
-    // .optional()
-    .refine((value) => value !== undefined, ERROR_COMMNETS.EMPTY_STRING),
-  applyingType: z.enum(
-    ['frontend', 'backend', 'design', 'ios', 'android', 'productOwner'],
-    { message: ERROR_COMMNETS.NON_CHECK }
-  ),
-});
-
-export type FormField = Partial<z.infer<typeof formSchema>>;
-
-const defaultForm: FormField = {
-  personalInfo: undefined,
-  name: undefined,
-  email: undefined,
-  phoneNumber: undefined,
-  applyingType: undefined,
-};
-const steps = createFunnelSteps<FormField>()
-  .extends('personalInfo')
-  .extends('basicInfo', { requiredKeys: 'personalInfo' })
-  .extends('applyingType', { requiredKeys: ['email', 'name', 'phoneNumber'] })
-  .build();
+import ApplyFunnel from './ApplyFunnel';
+import formSchema, { FormField } from '../../assets/form-info/schema';
+import defaultForm from '../../assets/form-info/defaultValues';
 
 const ApplyForm = () => {
-  const funnel = useFunnel({
-    id: 'applyform',
-    steps: steps,
-    initial: {
-      step: 'personalInfo',
-      context: defaultForm,
-    },
-  });
-
   const formMethods = useForm({
     resolver: zodResolver<FormField>(formSchema),
     defaultValues: defaultForm,
   });
-
-  // const onSubmit = (data: unknown) => mutate(data);
-  // const onSubmitFailed = () => toast(ERROR_COMMNETS.SUBMIT_FAILED); // post 동작에 사용
 
   const {
     isPending,
@@ -85,7 +21,7 @@ const ApplyForm = () => {
     mutate: submitData,
   } = useMutation({
     mutationFn: (data: FormField) => {
-      return new Promise((resolve, reject) => {
+      return new Promise((resolve) => {
         setTimeout(() => {
           console.log(data);
           resolve({ status: 200 });
@@ -107,88 +43,7 @@ const ApplyForm = () => {
     <>
       <FormProvider {...formMethods}>
         <form onSubmit={formMethods.handleSubmit((data) => submitData(data))}>
-          <funnel.Render
-            personalInfo={({ history, index }) => (
-              <StepLayout
-                stepIndex={index}
-                onNext={async () => {
-                  const isValid = await formMethods.trigger('personalInfo');
-                  if (isValid) {
-                    history.push('basicInfo', {
-                      personalInfo: formMethods.getValues('personalInfo')!,
-                    });
-                  }
-                }}
-              >
-                <PersonalInfo />
-              </StepLayout>
-            )}
-            basicInfo={({ context, history, index }) => (
-              <StepLayout
-                stepIndex={index}
-                onNext={async () => {
-                  const isFormValid = await formMethods.trigger([
-                    'name',
-                    'email',
-                    'phoneNumber',
-                  ]);
-                  if (isFormValid) {
-                    history.push('applyingType', (prev) => ({
-                      ...prev,
-                      name: formMethods.getValues('name')!,
-                      email: formMethods.getValues('email')!,
-                      phoneNumber: formMethods.getValues('phoneNumber')!,
-                    }));
-                  }
-                }}
-                onPrev={() => history.back()}
-                preValidate={async () => {
-                  const isFormValid = await formMethods.trigger('personalInfo');
-                  const isFormEqualToContext =
-                    context.personalInfo ===
-                    formMethods.getValues('personalInfo');
-                  if (!isFormValid || !isFormEqualToContext) {
-                    toast(ERROR_COMMNETS.INVALID_PROCESS);
-                    // alert('다시 작성하고 다음 버튼 클릭하세욧!');
-                    history.back();
-                    return;
-                  }
-                }}
-              >
-                <BasicInfo />
-              </StepLayout>
-            )}
-            applyingType={({ context, history, index }) => (
-              <StepLayout
-                isSubmit
-                stepIndex={index}
-                onPrev={() => history.back()}
-                preValidate={async () => {
-                  const target = ['name', 'email', 'phoneNumber'];
-
-                  const isFormValid = await formMethods.trigger(
-                    target as (keyof FormField)[]
-                  );
-
-                  const isFormEqualToContext = target.reduce((acc, cur) => {
-                    return (
-                      acc &&
-                      context[cur as keyof FormField] ===
-                        formMethods.getValues(cur as keyof FormField)
-                    );
-                  }, true);
-                  if (!isFormValid || !isFormEqualToContext) {
-                    toast(ERROR_COMMNETS.INVALID_PROCESS);
-                    // alert('다시 작성하세욧!');
-                    history.back();
-                    return;
-                  }
-                }}
-              >
-                <ApplyingType />
-              </StepLayout>
-            )}
-          />
+          <ApplyFunnel />
         </form>
       </FormProvider>
     </>
