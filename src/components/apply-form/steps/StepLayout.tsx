5012489b82e615dbd1bf5498711445a216bbf086
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-02-20 18:40:31 +0900

    feat: 퍼널 스텝 레이아웃 기능 제작 완료

--- src/components/apply-form/steps/StepLayout.tsx
+++ src/components/apply-form/steps/StepLayout.tsx
@@ -1,63 +1,42 @@
-import { ReactNode, useEffect, useState } from 'react';
-import { useFormContext } from 'react-hook-form';
+import { ReactNode, useEffect } from 'react';
+
+interface StepLayoutProps {
+  stepIndex: number;
+  preValidate?: () => void;
+  onNext?: () => void;
+  onPrev?: () => void;
+  isSubmit?: boolean;
+  children: ReactNode;
+}
 
 const StepLayout = ({
-  context,
+  stepIndex,
+  preValidate,
   onNext,
   onPrev,
+  isSubmit = false,
   children,
-}: {
-  context: object;
-  onNext?: () => Promise<void>;
-  onPrev?: () => void;
-  children: ReactNode;
-}) => {
-  const { setValue, getValues } = useFormContext();
-  // test
-  console.log('컨텍스트: ', context, getValues());
-
-  // useEffect(() => {
-  //   const formContent = getValues();
-  //   Object.entries(formContent).forEach(([key, value]) => {
-  //     const updatedValue = key in Object.keys(context) ? value : undefined;
-  //     setValue(key, updatedValue);
-  //   });
-  // }, []);
-
-  const [state, setState] = useState(0);
+}: StepLayoutProps) => {
+  useEffect(() => {
+    if (preValidate) preValidate();
+  }, []);
   return (
     <>
       <div>
-        <div>대충 단계</div>
+        <div>대충 단계: {stepIndex}</div>
         {children}
         {onPrev && (
-          <button
-            type="button"
-            onClick={() => {
-              onPrev();
-            }}
-          >
+          <button type="button" onClick={onPrev}>
             이전
           </button>
         )}
         {onNext && (
-          <button
-            type="button"
-            onClick={async () => {
-              await onNext();
-            }}
-          >
+          <button type="button" onClick={onNext}>
             다음
           </button>
         )}
+        {isSubmit && <button type="submit">제출</button>}
       </div>
-      <button
-        type="button"
-        onClick={() => {
-          setState((prev) => prev + 1);
-          console.log('스텝에서 값들: ', getValues());
-        }}
-      >{`리렌더링 버튼: ${state}`}</button>
     </>
   );
 };
